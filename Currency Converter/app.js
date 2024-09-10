f4c9d5d094a384928a2c62db6d98e92f6448320b
ar7em1s <slavina.ss94@gmail.com>
2024-09-10 17:11:52 +0300

    updt

--- Currency Converter/app.js
+++ Currency Converter/app.js
@@ -18,8 +18,27 @@ currencies.forEach((currency) => {
 });
 
 fromDropDown.value = "USD";
-fromDropDown.value = "BGN";
+toDropDown.value = "BGN";
 
 let convertCurrency = () => {
-    
-}
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+  if (amount.length != 0) {
+    fetch(api).then((response) =>
+      response.json().then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} `;
+      })
+    );
+  } else {
+    alert("Please fill in the amount!");
+  }
+};
+
+document.querySelector("#convert-button").addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
