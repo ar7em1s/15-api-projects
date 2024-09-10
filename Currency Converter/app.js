ed0cbb3b6879e2086a054394df4550739d39ddd8
ar7em1s <slavina.ss94@gmail.com>
2024-09-10 16:27:49 +0300

    style updt

--- Currency Converter/app.js
+++ Currency Converter/app.js
@@ -1,6 +1,6 @@
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const fromDropDown = document.getElementById("from-currency-select");
-const toDromDown = document.getElementById("to-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
 
 // dropdowns from the currencies list
 currencies.forEach((currency) => {
@@ -14,5 +14,5 @@ currencies.forEach((currency) => {
   const option = document.createElement("option");
   option.value = currency;
   option.text = currency;
-  toDromDown.add(option);
+  toDropDown.add(option);
 });
