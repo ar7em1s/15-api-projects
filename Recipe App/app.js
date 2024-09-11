94b6ab2f804895d2c48905169ec61fc7bc39cc3e
ar7em1s <slavina.ss94@gmail.com>
2024-09-11 14:40:14 +0300

    updt

--- Recipe App/app.js
+++ Recipe App/app.js
@@ -1,3 +1,35 @@
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let userInp = document.getElementById("user-input").value;
+
+fetch(url + "pizza")
+  .then((response) => response.json())
+  .then((data) => {
+    let myMeal = data.meals[0];
+    console.log(myMeal);
+    // console.log(myMeal.strMeal);
+    // console.log(myMeal.strArea);
+    // console.log(myMeal.strInstructions);
+    // console.log(myMeal.strMealThumb);
+
+    let count = 1;
+    let ingredients = [];
+
+    for (let i in myMeal) {
+      let ingredient = "";
+      let measure = "";
+
+      if (i.startsWith("strIngredient") && myMeal[i]) {
+        ingredient = myMeal[i];
+        measure = myMeal[`strMeasure` + count];
+        count++;
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    console.log(ingredients);
+
+    result.innerHTML = `<img src=${myMeal.strMealThumb}>
+    
+    `;
+  });
