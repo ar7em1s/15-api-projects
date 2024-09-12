b4a1acad115e97caed0f81d6a94865ac636fdf2b
ar7em1s <slavina.ss94@gmail.com>
2024-09-12 17:36:22 +0300

    url added

--- Gif Search App/app.js
+++ Gif Search App/app.js
@@ -1,1 +1,20 @@
 import { apiKey } from "./giphyApiKey.js";
+
+let submit = document.getElementById("submit-btn");
+
+let generateGif = () => {
+  //display loader animation until gifs load
+  let loader = document.querySelector(".loader");
+  loader.style.display = "block";
+  document.querySelector(".wrapper").style.display = "none";
+
+  //get the search value ('laugh' is default)
+  let q = document.getElementById("search-box").value;
+
+  //results
+  let gifCount = 10;
+  let finalUrl = `https://www.api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}`;
+};
+
+submit.addEventListener("click", generateGif);
+window.addEventListener("load", generateGif);
