5d20c0909035bde52ea3bca4b2c330f91044656e
ar7em1s <slavina.ss94@gmail.com>
2024-09-13 13:52:25 +0300

    init upld

---
+++ Country Guide App/app.js
@@ -0,0 +1,12 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInput = document.getElementById("country-input");
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInput.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
