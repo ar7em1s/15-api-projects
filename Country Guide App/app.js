48ddd6d6abe4854cb4d242adf2c5eaeee8af7c52
ar7em1s <slavina.ss94@gmail.com>
2024-09-13 15:07:37 +0300

    updt

--- Country Guide App/app.js
+++ Country Guide App/app.js
@@ -8,5 +8,36 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+
+      result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">
+      <h2>${data[0].name.common}</h2>
+      <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Capital:</h4>
+            <span>${data[0].capital[0]}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Continent:</h4>
+            <span>${data[0].continents[0]}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Population:</h4>
+            <span>${data[0].population.toLocaleString("en-US")}</span>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Currency:</h4>
+            <span>${data[0].currencies[Object.keys(data[0].currencies)].name} (${data[0].currencies[Object.keys(data[0].currencies)].symbol})</span>
+        </div>
+      </div>
+    `;
     });
 });
